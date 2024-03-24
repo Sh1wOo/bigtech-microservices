51b32e3939d921d9c514741ecf04865aa82d5f27
Sh1wOo <zuromr@gmail.com>
2024-03-25 00:28:22 +0300

    feat: base http client with chi and cashing

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,122 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-
 	"github.com/fatih/color"
+	"github.com/go-chi/chi/v5"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
 )
 
+const (
+	baseUrl       = "localhost:8080"
+	createPostfix = "/test"
+	getPostfix    = "/test/%d"
+)
+
+// NoteInfo Информация о заметке
+type NoteInfo struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+// Note Сама заметка
+type Note struct {
+	ID        int64     `json:"id"`
+	Info      NoteInfo  `json:"info"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+// SyncMap для кэша
+type SyncMap struct {
+	elems map[int64]*Note
+	m     sync.RWMutex
+}
+
+var notes = &SyncMap{
+	elems: make(map[int64]*Note),
+}
+
+func createTestHandler(w http.ResponseWriter, r *http.Request) {
+	info := &NoteInfo{}
+
+	if err := json.NewDecoder(r.Body).Decode(info); err != nil {
+		http.Error(w, "Failed to decode the data", http.StatusBadRequest)
+		return
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	now := time.Now()
+
+	note := &Note{
+		ID:        rand.Int63(),
+		Info:      *info,
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	if err := json.NewEncoder(w).Encode(note); err != nil {
+		http.Error(w, "Failed to encode", http.StatusInternalServerError)
+	}
+
+	notes.m.Lock()
+	defer notes.m.Unlock()
+
+	notes.elems[note.ID] = note
+}
+
+func parseNoteID(idStr string) (int64, error) {
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func getNoteHandler(w http.ResponseWriter, r *http.Request) {
+	noteID := chi.URLParam(r, "id")
+	id, err := parseNoteID(noteID)
+	if err != nil {
+		http.Error(w, "Invalid note ID", http.StatusBadRequest)
+		return
+	}
+
+	notes.m.RLock()
+	defer notes.m.RUnlock()
+
+	note, ok := notes.elems[id]
+	if !ok {
+		http.Error(w, "Note not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err = json.NewEncoder(w).Encode(note); err != nil {
+		http.Error(w, "Failed to encode note data", http.StatusInternalServerError)
+		return
+	}
+}
+
+// базовая имплементация http на golang
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	r := chi.NewRouter()
+
+	r.Post(createPostfix, createTestHandler)
+	r.Get(getPostfix, getNoteHandler)
+
+	fmt.Println(color.GreenString("Server listen on http://%s", baseUrl))
+	err := http.ListenAndServe(baseUrl, r)
+	if err != nil {
+		fmt.Println(color.RedString("Error"))
+	}
+
 }
